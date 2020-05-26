b70c59f128129234e902677b9e378d394e243312
Jordan Chadwick <jordan.chadwick@gmail.com>
2020-05-26 15:23:53 -0400

    adds better logging

--- upload_server.js
+++ upload_server.js
@@ -5,6 +5,7 @@ var compare = require('tsscmp');
 var http = require('http');
 var formidable = require('formidable');
 var fs = require('fs');
+var timestamp = require('time-stamp');
 
 // parse command line arguments
 if (process.argv.length < 3) {
@@ -24,6 +25,7 @@ var server = http.createServer(function (req, res) {
     var credentials = auth(req);
     // check credentials
     if (!credentials || !checkauth(credentials.name, credentials.pass)) {
+        log(req, "invalid login");
         res.statusCode = 401;
         res.setHeader('WWW-Authenticate', 'Basic realm="upload"');
         res.end('Access denied');
@@ -37,6 +39,7 @@ var server = http.createServer(function (req, res) {
                     if (err) throw err;
                 })
                 res.write(`File uploaded and moved to ${newpath}`);
+                log(req, `file ${files.filetoupload.name} uploaded`);
                 res.end();
             })
         } else {
@@ -58,6 +61,14 @@ function checkauth(name, pass) {
     valid = compare(pass, password) && valid;
 
     return valid;
+}
+
+// produces log output
+function log(req, msg) {
+    // Apache log format
+    //10.185.248.71 - - [09/Jan/2015:19:12:06 +0000] 808840 "GET /inventoryService/inventory/purchaseItem?userId=20253471&itemId=23434300 HTTP/1.1" 500 17 "-" "Apache-HttpClient/4.2.6 (java 1.5)"
+    timeString = timestamp('DD/MM/YYYY:HH:MM:SS');
+    console.log(`${req.connection.remoteAddress} - - [${timeString}] ${req.url} - ${msg}`);
 }
 
 // start server loop
