a0e05be6ab459bf931881199d0badf60b8ff522d
Jordan Chadwick <jordan.chadwick@gmail.com>
2020-05-26 13:40:40 -0400

    adds basic authentication (randomly generated)

--- upload_server.js
+++ upload_server.js
@@ -1,5 +1,7 @@
 // A simple server that accepts a file upload
 
+var auth = require('basic-auth');
+var compare = require('tsscmp');
 var http = require('http');
 var formidable = require('formidable');
 var fs = require('fs');
@@ -12,28 +14,51 @@ if (process.argv.length < 3) {
     uploadFolder = process.argv[2];
 }
 
-http.createServer(function (req, res) {
-    if (req.url == '/fileupload') {
-        var form = new formidable.IncomingForm();
-        form.parse(req, function (err, fields, files) {
-            var oldpath = files.filetoupload.path;
-            var newpath = uploadFolder + files.filetoupload.name;
-            fs.rename(oldpath, newpath, function (err) {
-                if (err) {
-                    console.log(`An error occurred: ${err}`);
-                }
+// generate credentials
+username = Math.random().toString(36).substring(2, 15);
+password = Math.random().toString(36).substring(2, 15);
+console.log(`Username: ${username}`);
+console.log(`Password: ${password}`);
+
+var server = http.createServer(function (req, res) {
+    var credentials = auth(req);
+    // check credentials
+    if (!credentials || !checkauth(credentials.name, credentials.pass)) {
+        res.statusCode = 401;
+        res.setHeader('WWW-Authenticate', 'Basic realm="upload"');
+        res.end('Access denied');
+    } else {
+        if (req.url == '/fileupload') {
+            var form = new formidable.IncomingForm();
+            form.parse(req, function (err, fields, files) {
+                var oldpath = files.filetoupload.path;
+                var newpath = uploadFolder + files.filetoupload.name;
+                fs.rename(oldpath, newpath, function (err) {
+                    if (err) throw err;
+                })
                 res.write(`File uploaded and moved to ${newpath}`);
                 res.end();
             })
-            res.write('File uploaded');
-            res.end();
-        })
-    } else {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-        res.write('<input type="file" name="filetoupload"><br>');
-        res.write('<input type="submit" value="Upload">');
-        res.write('</form>');
-        return res.end();
+        } else {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+            res.write('<input type="file" name="filetoupload"><br>');
+            res.write('<input type="submit" value="Upload">');
+            res.write('</form>');
+            return res.end();
+        }
     }
-}).listen(8080);
+});
+
+// function to check authentication
+function checkauth(name, pass) {
+    var valid = true;
+
+    valid = compare(name, username) && valid;
+    valid = compare(pass, password) && valid;
+
+    return valid;
+}
+
+// start server loop
+server.listen(8080);
